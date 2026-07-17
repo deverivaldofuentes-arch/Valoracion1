b285cb61fadd27d5caeeeb33877e5d03bfd27167
unknown <dev.erivaldofuentes@gmial.com>
2026-07-16 20:03:33 -0400

    Redirigir ruta raiz a modulo vehiculos

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('vehiculos.index');
 });
 
 use App\Http\Controllers\VehiculoController;
